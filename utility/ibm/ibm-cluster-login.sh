d16dfc439520a9cec4dc7e00832e67527e6359d3
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2022-06-02 20:59:30 +0000

    Updated ibm-cluster-login script
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- utility/ibm/ibm-cluster-login.sh
+++ utility/ibm/ibm-cluster-login.sh
@@ -1,1 +1,4 @@
-ibmcloud cs cluster config --cluster tcss702-test-bobs
+#!/bin/bash
+ibmcloud login -u schmitzr1984@gmail.com -r us-south && \
+ibmcloud ks cluster config --cluster tcss702-bobs-cluster || \
+echo Failed to login to ibmcloud
