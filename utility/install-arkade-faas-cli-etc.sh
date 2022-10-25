97de2b1f11a5aa64c6705780e3619347c8b9600f
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2022-10-25 01:03:42 -0700

    Remove terraform from installed packages
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- utility/install-arkade-faas-cli-etc.sh
+++ utility/install-arkade-faas-cli-etc.sh
@@ -23,9 +23,3 @@ curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/b
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm kubectl*
-
-# Install terraform
-sudo apt update && sudo apt install -y gnupg software-properties-common curl
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt update && sudo apt install -y terraform
