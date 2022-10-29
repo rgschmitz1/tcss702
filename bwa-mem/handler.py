c1b6d5cd3bfdb154cb4cf4c8ce87f5d412bdc14a
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2022-10-29 15:14:17 -0700

    Adding json input for bwa-mem function
    inputfile and bucket will be passed via json
    
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- bwa-mem/handler.py
+++ bwa-mem/handler.py
@@ -4,57 +4,56 @@ import os
 import subprocess
 from .Inspector import Inspector
 
-def handle(event, context):
+# Create Minio client
+def minio_client():
     with open("/var/openfaas/secrets/minio-access-key") as f:
         access_key = f.read()
     with open("/var/openfaas/secrets/minio-secret-key") as f:
         secret_key = f.read()
 
-    mc = Minio(os.environ['minio_hostname'],
-               access_key=access_key,
-               secret_key=secret_key,
-               secure=False)
+    return Minio(os.environ['minio_hostname'],
+        access_key=access_key,
+        secret_key=secret_key,
+        secure=False)
 
-    # Download normal and tumor samples
-    mc.fget_object('bwa-mem', 'normal.tar.xz', '/tmp/normal.tar.xz')
-    mc.fget_object('bwa-mem', 'tumor.tar.xz', '/tmp/tumor.tar.xz')
-
-    # Uncompress normal and tumor samples
-    uncompress_normal_sample = subprocess.run('tar -xf /tmp/normal.tar.xz -C /tmp', shell=True)
-    print(uncompress_normal_sample)
-    uncompress_tumor_sample = subprocess.run('tar -xf /tmp/tumor.tar.xz -C /tmp', shell=True)
-    print(uncompress_tumor_sample)
-
-    # Extract reference sequence and BWA index
-    uncompress_ref_seq = subprocess.run('tar -xf /home/app/function/genome/GRCh38.d1.vd1.fa.tar.gz -C /tmp', shell=True)
-    print(uncompress_ref_seq)
-    uncompress_bwa_idx = subprocess.run('tar -xf /home/app/function/genome/GRCh83.d1.vd1_BWA.tar.gz -C /tmp', shell=True)
-    print(uncompress_bwa_idx)
-
-    # Align normal
-    align_normal = subprocess.run('bwa mem -t 8 -T 0 /tmp/GRCh38.d1.vd1.fa /tmp/normal_1.fq /tmp/normal_2.fq | samtools sort -o /tmp/normal_realign.bam', shell=True)
-    print(align_normal)
-    # Align tumor
-    align_tumor = subprocess.run('bwa mem -t 8 -T 0 /tmp/GRCh38.d1.vd1.fa /tmp/tumor_1.fq /tmp/tumor_2.fq | samtools sort -o /tmp/tumor_realign.bam', shell=True)
-    print(align_tumor)
-
-    # Put aligned normal and tumor bam in minio
-    mc.fput_object('bwa-mem', 'normal_realign.bam', '/tmp/normal_realign.bam')
-    mc.fput_object('bwa-mem', 'tumor_realign.bam', '/tmp/tumor_realign.bam')
-
-    # Collect data
+def handle(event, context):
+    # Start inspector
     inspector = Inspector()
     inspector.inspectAll()
 
-    # Add custom message and finish the function
-#    if "startWallClock" in event:
-#        inspector.addAttribute("startWallClock", event['startWallClock'])
-
+    # Parse input json data
     body = json.loads(event.body)
 
+    # assume input file file is tar and compressed
+    inputfile = body['inputfile']
+    bucket = body['bucket']
+
+    # Get minio client object to download/upload data
+    mc = minio_client()
+
+    # Download sample
+    mc.fget_object(bucket, inputfile, f'/tmp/{inputfile}')
+
+    # Uncompress sample
+    uncompress_sample = subprocess.run(f'tar -xf /tmp/{inputfile} -C /tmp', shell=True)
+    print(uncompress_sample, flush=True)
+
+    # Extract BWA index
+    uncompress_bwa_idx = subprocess.run('tar -xf /home/app/function/genome/GRCh83.d1.vd1_BWA.tar.gz -C /tmp', shell=True)
+    print(uncompress_bwa_idx, flush=True)
+
+    # Align sample and convert to bam
+    inputfile = inputfile.split('.')[0]
+    fastq = [f'{inputfile}_1.fq', f'{inputfile}_2.fq']
+    aligned_bam = f'{inputfile}_realign.bam'
+    align_sample = subprocess.run(f'bwa mem -t 8 -T 0 /tmp/GRCh38.d1.vd1.fa /tmp/{fastq[0]} /tmp/{fastq[1]} | samtools sort -o /tmp/{aligned_bam}', shell=True)
+    print(align_sample, flush=True)
+
+    # Put aligned normal and tumor bam in minio
+    mc.fput_object(bucket, aligned_bam, f'/tmp/{aligned_bam}')
+
+    # Collect inspector deltas
     inspector.inspectAllDeltas()
-    # Include functionName
-#    inspector.addAttribute("functionName", fn[body['fn']].__name__)
 
     iret = inspector.finish()
     ret = {
--- utility/invoke-bwa-mem.sh
+++ utility/invoke-bwa-mem.sh
@@ -3,7 +3,7 @@ cd $(dirname $0)
 . color-prompt.sh
 
 execute_bwa-mem_fn() {
-	if curl -H "Content-Type: application/json" -X POST http://localhost:8080/function/bwa-mem; then
+	if curl -H "Content-Type: application/json" -X POST -d "$1" http://localhost:8080/function/bwa-mem; then
 		sleep 2
 	else
 		prompt_error "Failed to execute $1"
@@ -11,7 +11,9 @@ execute_bwa-mem_fn() {
 	fi
 }
 
-prompt_info "Invoking BWA-MEM function"
-execute_bwa-mem_fn
+prompt_info "BWA-MEM function, align normal sample"
+execute_bwa-mem_fn '{"inputfile":"normal.tar.xz", "bucket":"bwa-mem"}'
+prompt_info "BWA-MEM function, align tumor sample"
+execute_bwa-mem_fn '{"inputfile":"tumor.tar.xz", "bucket":"bwa-mem"}'
 
 printf "\nTotal runtime: $SECONDS\n"
