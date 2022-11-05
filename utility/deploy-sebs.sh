87846cf8ba85beefe348eb17c1997e320e37c89a
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2022-11-04 22:21:01 -0700

    Added script to deploy SeBS functions
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

---
+++ utility/deploy-sebs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+./pass-minio-secrets.sh || exit $?
+
+bucket=minio/sebs
+key=bacillus_subtilis.fasta
+
+# Make SeBS bucket
+mc ls $bucket || mc mb $bucket
+
+# Move data into bucket for SeBS
+if [ -z "$(mc ls $bucket/$key)" ] && ! mc cp ../data/$key $bucket; then
+	printf "\nERROR: failed to copy $key to $bucket\n"
+	exit 1
+fi
+
+cd ..
+
+faas-cli up -f bwa-mem.yml
+exit $?
