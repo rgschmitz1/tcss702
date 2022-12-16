ca0502df650b7484bb60f95a7437f13106a2dc51
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2022-12-16 11:45:00 -0800

    Added Jupyter in docker script
    - Running Jupyter in docker will avoid messy setup on the client
    - Added configuration step to awscli otherwise the cli is useless
    
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- utility/install-awscli.sh
+++ utility/install-awscli.sh
@@ -3,6 +3,13 @@
 # Check if awscli is already installed
 which aws > /dev/null && exit 0
 
+. $(dirname $0)/color-prompt.sh
+
+cleanup() {
+	rm -fr awscliv2.zip aws
+	exit $1
+}
+
 # Check if unzip is installed
 if ! which unzip > /dev/null; then
 	sudo apt update && \
@@ -10,8 +17,17 @@ if ! which unzip > /dev/null; then
 	exit 1
 fi
 
+prompt_info "Installing awscli (v2)"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
 cd /tmp
 unzip awscliv2.zip
-sudo ./aws/install
-exit $?
+if ! sudo ./aws/install; then
+	prompt_error "Failed to install aws cli"
+	cleanup 1
+fi
+
+# Configure awscli
+prompt_info "Please enter AWS credentials"
+aws configure
+
+cleanup $?
