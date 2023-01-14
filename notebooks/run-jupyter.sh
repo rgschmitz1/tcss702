255674309925e042d71934b397eb3c389ff1429f
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2023-01-13 23:46:55 -0800

    Using docker-compose for Jupyter notebook
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- notebooks/run-jupyter.sh
+++ notebooks/run-jupyter.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-cd $(dirname $0)/..
+trap cleanup INT
 
-. utility/color-prompt.sh
+cleanup() {
+	prompt_info "Entering cleanup function"
+	docker compose down
+	exit $1
+}
 
-./utility/install-docker.sh || exit $?
+cd $(dirname $0)
+
+. ../utility/color-prompt.sh
+
+../utility/install-docker.sh || exit $?
 
 if [ -z "$(docker ps -f "name=notebook" -q)" ]; then
 	prompt_info "Starting Jupyter as daemon"
-	if ! docker run --rm --name notebook -v $PWD:/home/jovyan/work -p 8888:8888 -d jupyter/scipy-notebook:python-3.10.8; then
-		prompt_error "Failed to start Jupyter"
+	docker compose up
+else
+	# Get Jupyter URL
+	url=$(docker logs notebook 2> /dev/stdout | awk '/127.0.0.1/ {print $NF; exit}')
+	if [ -z "$url" ]; then
+		prompt_error "Failed to get Jupyter URL"
 		exit 1
 	fi
-	sleep 3
-fi
+	prompt_info "Connect to Jupyter using:"
+	echo $url
 
-# Get Jupyter URL
-url=$(docker logs notebook 2> /dev/stdout | awk '/127.0.0.1/ {print $NF; exit}')
-if [ -z "$url" ]; then
-	prompt_error "Failed to get Jupyter URL"
-	exit 1
+	# If xdg-open is installed, open Jupyter URL automatically
+	which xdg-open > /dev/null && xdg-open $url
 fi
-prompt_info "Connect to Jupyter using:"
-echo $url
-
-# If xdg-open is installed, open Jupyter URL automatically
-which xdg-open > /dev/null && xdg-open $url
