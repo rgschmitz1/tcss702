23ca63ab8bbd0bdb8061758699df43bbd7fbd547
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2023-02-26 22:30:06 -0800

    Cleaning up utility scripts
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- utility/install-kind.sh
+++ utility/install-kind.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+which kind > /dev/null && exit 0
 cd $(dirname $0)
 . color-prompt.sh
-
-which kind > /dev/null && exit 0
 prompt_info "Installing kind (v0.17.0)"
 curl -sLo kind "https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64" && \
 chmod +x kind && \
--- utility/install-zstd.sh
+++ utility/install-zstd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-. $(dirname $0)/color-prompt.sh
 which zstd > /dev/null && exit 0
+. $(dirname $0)/color-prompt.sh
 prompt_info "Installing Zstandard"
 sudo apt update && sudo apt install -y zstd
 exit $?
